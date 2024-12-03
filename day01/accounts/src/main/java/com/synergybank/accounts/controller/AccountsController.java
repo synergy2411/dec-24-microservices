cd60ef151c565521547a9088fb2a1212bb66b721
Sumit Khandelwal <synergy2411@outlook.com>
2024-12-03 10:16:13 +0530

    validation added

--- day01/accounts/src/main/java/com/synergybank/accounts/controller/AccountsController.java
+++ day01/accounts/src/main/java/com/synergybank/accounts/controller/AccountsController.java
@@ -3,41 +3,51 @@ package com.synergybank.accounts.controller;
 import com.synergybank.accounts.dto.CustomerDto;
 import com.synergybank.accounts.dto.ResponseDto;
 import com.synergybank.accounts.service.IAccountsService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
+@Validated
 public class AccountsController {
 
     private IAccountsService iAccountsService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> create(@RequestBody CustomerDto customerDto){
+    public ResponseEntity<ResponseDto> create(@RequestBody @Valid CustomerDto customerDto){
         iAccountsService.createAccount(customerDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ResponseDto("Success", HttpStatus.CREATED));
     }
 
     @GetMapping("/fetch")
-    public ResponseEntity<CustomerDto> fetch(@RequestParam String mobileNumber){
+    public ResponseEntity<CustomerDto> fetch(@RequestParam
+                                                 @Pattern(regexp = "^$|[0-9]{10}", message = "Mobile number should have 10 digits")
+                                                 String mobileNumber){
         CustomerDto customerDto = iAccountsService.fetchCustomer(mobileNumber);
         return ResponseEntity.status(200).body(customerDto);
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto> delete(@RequestParam String mobileNumber){
+    public ResponseEntity<ResponseDto> delete(@RequestParam
+                                                  @Pattern(regexp = "^$|[0-9]{10}", message = "Mobile number should have 10 digits")
+                                                  String mobileNumber){
         iAccountsService.deleteAccount(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new ResponseDto("Deleted", HttpStatus.OK));
     }
 
     @PatchMapping("/update")
-    public ResponseEntity<ResponseDto> update(@RequestBody CustomerDto customerDto,
-                                              @RequestParam String mobileNumber){
+    public ResponseEntity<ResponseDto> update(@RequestBody @Valid CustomerDto customerDto,
+                                              @RequestParam
+                                              @Pattern(regexp = "^$|[0-9]{10}", message = "Mobile number should have 10 digits")
+                                              String mobileNumber){
         iAccountsService.updateAccount(mobileNumber, customerDto);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(
                 new ResponseDto("Updated", HttpStatus.ACCEPTED)
--- day01/accounts/src/main/java/com/synergybank/accounts/entity/Customer.java
+++ day01/accounts/src/main/java/com/synergybank/accounts/entity/Customer.java
@@ -1,6 +1,9 @@
 package com.synergybank.accounts.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +19,13 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long customerId;
+
+    @NotEmpty(message = "Customer name is can not be empty")
     private String name;
+
+    @Email(message = "Customer email should be in proper format")
     private String email;
+
+    @Pattern(regexp = "^$|[0-9]{10}", message = "Mobile number should have 10 digits")
     private String mobileNumber;
 }
