a4957eef3f5a1c7cfbfb79ee0c5d2a543b30b5be
Sumit Khandelwal <synergy2411@outlook.com>
2024-12-04 15:08:52 +0530

    route locator added

--- day03_b/gatewayserver/src/main/java/com/synergybank/gatewayserver/GatewayserverApplication.java
+++ day03_b/gatewayserver/src/main/java/com/synergybank/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,9 @@ package com.synergybank.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +13,25 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder
+				.routes()
+				.route(p -> p
+						.path("/synergybank/accounts/**")
+						.filters(f -> f.rewritePath("/synergybank/accounts/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTS")
+				)
+				.route(p -> p
+						.path("/synergybank/cards/**")
+						.filters(f -> f.rewritePath("/synergybank/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS")
+				)
+				.route(p -> p
+						.path("/synergybank/loans/**")
+						.filters(f -> f.rewritePath("/synergybank/loans/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOANS")
+				)
+				.build();
+	}
 }
